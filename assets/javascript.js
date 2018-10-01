c46082310981bf3efa226d8162681156611ab4c7
Hayley Hartman <hayleyhartman@Hayleys-MacBook-Pro.local>
2018-09-30 20:06:25 -0400

    sunday night

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,23 +1,34 @@
-$(".contactDiv").hide()
 $(".portfolioDiv").hide()
+$(".resumeDiv").hide()
 
-$(".contact").on("click", function(){
+$(".about").on("click", function(){
+    $(".aboutDiv").show()
     $(".contactDiv").show()
+    $(".portfolioDiv").hide()
+    $(".resumeDiv").hide()
+})
+
+$(".name").on("click", function(){
+    $(".aboutDiv").show()
+    $(".contactDiv").show()
+    $(".portfolioDiv").hide()
+    $(".resumeDiv").hide()
+
+})
+
+$(".resume").on("click", function(){
     $(".aboutDiv").hide()
     $(".portfolioDiv").hide()
+    $(".contactDiv").hide()
+    $(".resumeDiv").show()
 })
 
 $(".portfolio").on("click", function(){
     $(".portfolioDiv").show()
     $(".contactDiv").hide()
     $(".aboutDiv").hide()
-    
+    $(".resumeDiv").hide() 
 })
 
-$(".about").on("click", function(){
-    $(".aboutDiv").show()
-    $(".contactDiv").hide()
-    $(".portfolioDiv").hide()
 
-})
 
